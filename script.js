8033d20b9683f89ced01e727624d0c159fa0e273
CarliniBreno <marcondesbreno20@gmail.com>
2021-12-03 22:22:12 -0300

    Etapa Final Completa

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+    var enter = document.getElementById('nova-tarefa');
+
+    enter.addEventListener('keyup', (e) => {
+
+        if (e.key == "Enter") {
+            adicionarT()
+    }
+})
+
 function adicionarT() {
+    var content = document.getElementById("nova-tarefa");
+
     var areaTemplate = document.getElementById("template");
 
-    var content = document.getElementById("nova-tarefa").value;
+    var newItem = areaTemplate.content.cloneNode(true);
 
-    var novoItem = areaTemplate.content.cloneNode(true);
+    var listItem = newItem.querySelector("li");
+    
+    listItem.append(content.value)
 
-    novoItem.querySelector("li").append(content)
+    var list = document.getElementById("lista");
 
-    var lista = document.getElementById("lista");
-
-    lista.appendChild(novoItem);
+    if (content.value == "") {
+        alert("Digita alguma coisa aí, mula");
+    }
+    else {
+        var itemNum = list.children.length
+        if (itemNum % 2 == 0){
+            listItem.style.backgroundColor = "#E4EFF1"
+    }
+        if (itemNum % 3 == 0) {
+            listItem.style.color = "red"
+        }
+        list.append(newItem);
+        content.value = "";
+    }
 }
 
 
